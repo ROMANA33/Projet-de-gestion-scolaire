a43b7f8384dedfd32cae660a4be5a5afa851833e
Darkness <134399000+ROMANA33@users.noreply.github.com>
2024-04-28 00:51:03 +0000

    Add files via upload

---
+++ fichier PDF.java
@@ -0,0 +1,27 @@
+
+import java.io.File;
+//import com.google.zxing.qrcode.QRCodeWriter;
+
+class etudiant{
+
+    static String abdou;
+    static String ba;
+    static int age;
+    static String mail;
+
+    public class main{
+public static void main(String[] args) {
+        
+        String nom = abdou;
+        String prenom=ba;
+        int  age=22;
+        String mail;
+
+String etudiant = "nom,prenom,age,mail.pdf";
+String imageFormat = "jpg";
+
+String pdfnomfichier = "fichierPDf.java";
+String qrFileName = "etudiant_qr.jpg";
+}
+}
+}
